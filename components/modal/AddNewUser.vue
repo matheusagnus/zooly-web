0c97c0d35089c774d7f59c04971210a03f9811a7
matheusagnus <ferreira.zx@gmail.com>
2020-05-24 17:32:30 -0300

    :sparkles: add new modal to create new user

---
+++ components/modal/AddNewUser.vue
@@ -0,0 +1,123 @@
+<template>
+  <div>
+    <modal-template v-show="toggle" class="override-modal">
+      <template #header>
+        <b-icon icon="close" @click.native="toggleInfoModal()" />
+        <h3>Criar Nova Pessoa</h3>
+      </template>
+      <template #body>
+        <form>
+          <b-field label="Nome">
+            <b-input placeholder="Digite aqui..." />
+          </b-field>
+          <b-field label="Sobrenome">
+            <b-input placeholder="Digite aqui..." />
+          </b-field>
+          <b-field label="E-mail">
+            <b-input placeholder="Digite aqui..." />
+          </b-field>
+          <b-field label="Permissão">
+            <b-select placeholder="Selecione uma permissão">
+                <option
+                    v-for="option in data"
+                    :key="option.role">
+                    {{ option.title }}
+                </option>
+            </b-select>
+        </b-field>
+        </form>
+      </template>
+      <template #footer class="columns is-centered">
+        <div class="action-modal">
+          <b-button class="btn-secundary">Criar</b-button>
+        </div>
+      </template>
+    </modal-template>
+  </div>
+</template>
+
+<script>
+import ModalTemplate from "../shared/ModalTemplate";
+export default {
+  name: "AddNewUser",
+  components: { ModalTemplate },
+  data() {
+    return {
+      data: [
+        { role: 1, title: 'Admin' },
+        { role: 2, title: 'Gestor' },
+        { role: 3, title: 'Médico' },
+        { role: 4, title: 'Funcionário' }
+      ],
+      option: null
+    };
+  },
+  computed: {
+    filteredDataArray() {
+      return this.data.filter(option => {
+        return (
+          option
+            .toString()
+            .toLowerCase()
+            .indexOf(this.name.toLowerCase()) >= 0
+        );
+      });
+    }
+  },
+  props: {
+    toggle: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    toggleInfoModal() {
+      this.$emit("closeModal");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/stylesheet/base";
+.override-modal /deep/ .modal-box {
+  width: 100%;
+  max-width: 25rem;
+  height: 100%;
+  max-height: 36rem;
+  .modal-body {
+    max-height: 100%;
+  }
+}
+
+.control /deep/.help.counter {
+  display: none;
+}
+.action-modal {
+  display: flex;
+  justify-content: flex-end;
+  .btn-secundary {
+    margin-top: 1rem;
+    height: 2.5rem;
+    width: 33%;
+    font-weight: 700;
+    color: $primary;
+    border: 0.05rem solid $primary;
+
+    &:hover {
+      background: linear-gradient(
+        to right,
+        rgba(255, 114, 95, 1) 30%,
+        rgba(242, 49, 165, 1) 100%
+      );
+      color: #fafafa;
+    }
+  }
+}
+
+@media (max-width: 64rem) {
+  .override-modal /deep/ .modal-box {
+    max-height: 33rem;
+  }
+}
+</style>
--- pages/people/index.vue
+++ pages/people/index.vue
@@ -6,45 +6,50 @@
         <p class="subtitle">Todas as pessoas que têm acesso a aplicação</p>
       </div>
       <div class="action">
-        <b-button type="is-danger" icon-left="plus" rounded @click="toggleNewTask()">Criar pessoa</b-button>
+        <b-button type="is-danger" icon-left="plus" rounded @click="toggleNewUser()">Criar pessoa</b-button>
       </div>
       <hr />
     </div>
 
     <div class="people-table">
-      <b-table
-      :data="data"
-      :loading="isLoading"
-      :checkable="false"
-      :paginated="false"
-    >
-      <!-- eslint-disable-next-line -->
-      <template slot-scope="columns">
-        <b-table-column field="name" label="Nome">
-          {{ columns.row.name }} {{ columns.row.lastname }}
-        </b-table-column>
-        <b-table-column field="email" label="E-mail">
-          {{ columns.row.email }}
-        </b-table-column>
-        <b-table-column field="role" label="Permissão">
-          {{ columns.row.role }}
-        </b-table-column>
-        <b-table-column field="action" label="Ações">
-          <b-icon type="is-danger" icon="pencil" />
-          <b-icon type="is-danger" icon="delete" />
-        </b-table-column>
-      </template>
-    </b-table>
+        <b-table
+          :data="data"
+          :loading="isLoading"
+          :checkable="false"
+          :paginated="false"
+        >
+        <template slot-scope="columns">
+          <b-table-column field="name" label="Nome">
+            {{ columns.row.name }} {{ columns.row.lastname }}
+          </b-table-column>
+          <b-table-column field="email" label="E-mail">
+            {{ columns.row.email }}
+          </b-table-column>
+          <b-table-column field="role" label="Permissão">
+            {{ columns.row.role }}
+          </b-table-column>
+          <b-table-column field="action" label="Ações">
+            <b-icon type="is-danger" icon="pencil" />
+            <b-icon type="is-danger" icon="delete" />
+          </b-table-column>
+        </template>
+      </b-table>
     </div>
+
+    <add-new-user :toggle="newUser" @closeModal="toggleNewUser()" />
   </section>
 </template>
 
 <script>
+import AddNewUser from "@/components/modal/AddNewUser";
+
   export default {
     name: 'People',
+    components: { AddNewUser },
     data () {
       return {
         isLoading: false,
+        newUser: false,
         data: [
           { name: 'Matheus', lastname: 'Ferreira', email: 'ferreira.zx@gmail.com', role: 'Admin' },
           { name: 'Angleby', lastname: 'Vamberg', email: 'angleby@gmail.com', role: 'Gestor' },
@@ -53,6 +58,11 @@
         ]
       }
     },
+    methods: {
+      toggleNewUser () {
+        this.newUser = !this.newUser
+      }
+    }
   }
 </script>
 
